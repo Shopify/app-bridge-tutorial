f31d3546f54ab8657277883776da2b11fbae578d
Iain Campbell <iain.campbell@shopify.com>
2019-06-07 16:42:18 -0400

    Remove next, use koa directly with templating and static asset serving

---
+++ assets/app.js
@@ -0,0 +1,1 @@
+console.log("hi", SHOPIFY_API_KEY);
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require("isomorphic-fetch");
 const Koa = require("koa");
-const next = require("next");
+const route = require("koa-route");
+const views = require("koa-views");
+const static = require("koa-static");
+const mount = require("koa-mount");
+
 const { default: createShopifyAuth } = require("@shopify/koa-shopify-auth");
 const dotenv = require("dotenv");
 const { verifyRequest } = require("@shopify/koa-shopify-auth");
@@ -10,37 +14,45 @@ dotenv.config();
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
 
-app.prepare().then(() => {
-  const server = new Koa();
-  server.use(session(server));
-  server.keys = [SHOPIFY_API_SECRET_KEY];
+const server = new Koa();
+server.use(session(server));
+server.keys = [SHOPIFY_API_SECRET_KEY];
 
-  server.use(
-    createShopifyAuth({
-      apiKey: SHOPIFY_API_KEY,
-      secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ["read_products"],
-      afterAuth(ctx) {
-        const { shop, accessToken } = ctx.session;
-        ctx.cookies.set("shopOrigin", shop, { httpOnly: false });
-        ctx.redirect("/");
-      }
-    })
-  );
+server.use(
+  createShopifyAuth({
+    apiKey: SHOPIFY_API_KEY,
+    secret: SHOPIFY_API_SECRET_KEY,
+    scopes: ["read_products"],
+    afterAuth(ctx) {
+      const { shop, accessToken } = ctx.session;
+      ctx.cookies.set("shopOrigin", shop, { httpOnly: false });
+      ctx.redirect("/");
+    }
+  })
+);
 
-  server.use(verifyRequest());
-  server.use(async ctx => {
-    await handle(ctx.req, ctx.res);
-    ctx.respond = false;
-    ctx.res.statusCode = 200;
-  });
+server.use(verifyRequest());
 
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+server.use(
+  views(__dirname + "/views", {
+    map: {
+      html: "underscore"
+    }
+  })
+);
+
+server.use(
+  route.get("/", async function(ctx) {
+    ctx.state = { SHOPIFY_API_KEY };
+    await ctx.render("index");
+  })
+);
+
+server.use(mount("/assets", static(__dirname + "/assets")));
+
+server.listen(port, () => {
+  console.log(`> Ready on http://localhost:${port}`);
 });
